fc438bdd7e7352fea6624b7886cf22b843b135e6
abangura2 <abangura2@acamdemic.rrc.ca>
2024-03-29 17:14:36 -0500

    Initial commit: Set up project directory and file structure

---
+++ tests/test_chatbot.py
@@ -0,0 +1,9 @@
+"""
+Description: Test for chatbot application
+Author: AbdulRahman Hassan Bangura 
+Date: 2024-03-29
+Usage: From the console: python src/chatbot.py
+"""
+import unittest
+from unittest.mock import patch
+
