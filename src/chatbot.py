21f2e4900bf5cb6ed49a75b8e5b7149e001c5b37
abangura2 <abangura2@acamdemic.rrc.ca>
2024-03-31 15:10:06 -0500

    Implement get_account function in chatbot.py

--- src/chatbot.py
+++ src/chatbot.py
@@ -21,81 +21,145 @@ VALID_TASKS = {"balance", "deposit", "exit"}
 
 def get_account() -> int:
 
+    """
+    Get the user's account number.
+    """
 
-## Input for account 
+     # Account number input.
+    account_number = input("Please enter your account number: ")
 
-## REQUIRES REVISION
+    try:
+        # Convert the input string into an integer.
+        account_number = int(account_number)
+    
+    except ValueError:
+        # Notify the user and re-raise the exception.
+        raise ValueError("Account number must be a whole number.")
+    
+    # Check if the account number exists in the ACCOUNTS dictionary.
+    if account_number not in ACCOUNTS:
+        raise ValueError("Account number does not exist.")
+    
+    # Return the valid account number.
+    return account_number
+
+def get_amount() -> float:
+    """
+    Get the transaction amount from the user.
+    """
+    # Prompt for amount input.
+    amount = input("Enter the transaction amount: ")
+    
+    try:
+        # Convert the input string to a float.
+        amount = float(amount)
+
+        if amount <= 0:
+            raise ValueError("Invalid amount. Please enter a positive number.")
+    
+    except ValueError as e:
+        if "could not convert string to float" in str(e):
+            raise ValueError("Invalid amount. Amount must be numeric.") 
+        else:
+            raise 
+
+    # Return the validated amount as a float.
+    return amount
+
+def get_balance(account: int) -> str:
+    """
+    Get the balance for the given account.
+    """
+    # Check if the account exists in the ACCOUNTS dictionary.
+    if account not in ACCOUNTS:
+        raise ValueError("Account number does not exist.")
+    
+    balance = ACCOUNTS[account]["balance"]
+    return f"Your current balance for account {account} is ${balance:,.2f}."
+
+def make_deposit(account: int, amount: float) -> str:
+    """
+    Make a deposit into the specified account.
+    """
+    # Check if the account exists in the ACCOUNTS dictionary.
+    if account not in ACCOUNTS:
+        raise ValueError("Account number does not exist.")
+    
+    if amount <= 0:
+        raise ValueError("Invalid Amount. Amount must be positive.")
+    
+    # Update the account balance.
+    ACCOUNTS[account]["balance"] += amount
+
+    return f"You have made a deposit of ${amount:,.2f} to account {account}."
+
+def user_selection() -> str:
+    """
+    Prompt the user to select a task (balance, deposit, or exit).
+    """
+    selection = input("What would you like to do (balance/deposit/exit)? ")
+    
+    # Convert input to lowercase.
+    selection = selection.lower()
+    
+    if selection not in VALID_TASKS:
+        raise ValueError("Invalid task. Please choose balance, deposit, or exit.")
+    
+    # Return the valid selection.
+    return selection
+
+## GIVEN CHATBOT FUNCTION
 
 def chatbot():
-    '''
-    The main program.  Uses the functionality of the functions:
-        get_account()
-        get_amount()
-        get_balance()
-        make_deposit()
-        user_selection()
-    '''
-
-    print("Welcome! I'm the PiXELL River Financial Chatbot!  Let's get chatting!")
-
-    keep_going = True
-    while keep_going:
+    """
+    Main chatbot function to interact with the user.
+    """
+    print("Welcome! I'm the PiXELL River Financial Chatbot! Let's get chatting!")
+    
+    while True:
         try:
-            ## CALL THE user_selection FUNCTION HERE 
-            ## CAPTURING THE RESULTS IN A VARIABLE CALLED
-            ## selection:
-
-
-            if selection != "exit":
-                
-                # Account number validation.
-                valid_account = False
-                while valid_account == False:
+            # Prompt the user to select a task.
+            selection = user_selection()
+            
+            if selection == "exit":
+                print("Thank you for banking with PiXELL River Financial.")
+                break
+            
+            elif selection == "balance":
+                # Prompt for the account number.
+                while True:
                     try:
-                        ## CALL THE get_account FUNCTION HERE
-                        ## CAPTURING THE RESULTS IN A VARIABLE 
-                        ## CALLED account:
-
-
-                        valid_account = True
+                        account = get_account()
+                        break
                     except ValueError as e:
-                        # Invalid account.
                         print(e)
-                if selection == "balance":
-                        ## CALL THE get_balance FUNCTION HERE
-                        ## PASSING THE account VARIABLE DEFINED 
-                        ## ABOVE, AND PRINT THE RESULTS:
-
-                else:
-
-                    # Amount validation.
-                    valid_amount = False
-                    while valid_amount == False:
-                        try:
-                            ## CALL THE get_amount FUNCTION HERE
-                            ## AND CAPTURE THE RESULTS IN A VARIABLE 
-                            ## CALLED amount:
-
-
-                            valid_amount = True
-                        except ValueError as e:
-                            # Invalid amount.
-                            print(e)
-                ## CALL THE make_deposit FUNCTION HERE PASSING THE 
-                ## VARIABLES account AND amount DEFINED ABOVE AND 
-                ## PRINT THE RESULTS:
-
-
-            else:
-                # User selected 'exit'
-                keep_going = False
+                
+                # Retrieve and print the account balance.
+                print(get_balance(account))
+            
+            elif selection == "deposit":
+                # Prompt for the account number.
+                while True:
+                    try:
+                        account = get_account()
+                        break
+                    except ValueError as e:
+                        print(e)
+                
+                # Prompt for the deposit amount.
+                while True:
+                    try:
+                        amount = get_amount()
+                        break
+                    except ValueError as e:
+                        print(e)
+                        continue
+                
+                # Make the deposit and print the result.
+                print(make_deposit(account, amount))
+        
         except ValueError as e:
-            # Invalid selection:
             print(e)
 
-    print("Thank you for banking with PiXELL River Financial.")
-"""
-    
-"""
 if __name__ == "__main__":
     chatbot()
--- tests/test_chatbot.py
+++ tests/test_chatbot.py
@@ -10,3 +10,186 @@ import unittest
 # For mocking input
 from unittest.mock import patch
 
+
+# For functions and variables from the chatbot module
+from src.chatbot import get_account, get_amount, get_balance, make_deposit, user_selection, ACCOUNTS, VALID_TASKS
+
+class ChatbotTests(unittest.TestCase):
+
+     # Test the get_account function with valid account number
+    @patch('builtins.input')
+    def test_get_account_valid_account(self, mock_input):
+
+        # Mock user input "123456"
+        mock_input.side_effect = ["123456"]
+        self.assertEqual(get_account(), 123456)
+
+    # Test the get_account function with non-numeric input
+    @patch('builtins.input')
+    def test_get_account_non_numeric_account(self, mock_input):
+
+        # Mock input non-numeric string
+        mock_input.side_effect = ["non_numeric_data"]
+
+        # Expect a ValueError
+        with self.assertRaises(ValueError) as context:
+            get_account()
+
+        # Check that the exception message
+        self.assertEqual(str(context.exception), "Account number must be a whole number.")
+
+    # Test the get_account function with an account number that does not exist
+    @patch('builtins.input')
+    def test_get_account_account_does_not_exist(self, mock_input):
+        
+        # Mock input with a non-existing account number
+        mock_input.side_effect = ["112233"]
+
+        # Expect a ValueError
+        with self.assertRaises(ValueError) as context:
+            get_account()
+
+        # Check that the exception message
+        self.assertEqual(str(context.exception), "Account number does not exist.")
+    
+    # Test the get_amount function with valid numeric input
+    @patch('builtins.input')
+    def test_get_amount_valid_amount(self, mock_input):
+
+        # Mock user input with a valid amount
+        mock_input.side_effect = ["500.01"]
+        self.assertEqual(get_amount(), 500.01)
+
+    # Test the get_amount function with non-numeric input
+    @patch('builtins.input')
+    def test_get_amount_non_numeric(self, mock_input):
+
+        # Mock input as a non-numeric string
+        mock_input.side_effect = ["non_numeric_data"]
+
+        # Expect a ValueError
+        with self.assertRaises(ValueError) as context:
+            get_amount()
+        
+        # Check that the exception message
+        self.assertEqual(str(context.exception), "Invalid amount. Amount must be numeric.")
+
+    # Test the get_amount function with a negative input
+    @patch('builtins.input')
+    def test_get_amount_negative(self, mock_input):
+
+        # Mock user input with a negative number
+        mock_input.side_effect = ["-1"]
+
+        # Expect a ValueError
+        with self.assertRaises(ValueError) as context:
+            get_amount()
+        
+        # Check that the exception message
+        self.assertEqual(str(context.exception), "Invalid amount. Please enter a positive number.")
+
+    # Test the get_balance function with a valid account number
+    def test_get_balance_correct(self):
+
+        # Define a valid account number
+        account_number = 123456
+        expected_output = "Your current balance for account 123456 is $1,000.00."
+        self.assertEqual(get_balance(account_number), expected_output)
+
+
+    # Test the get_balance function with a non-existing account number
+    def test_get_balance_account_does_not_exist(self):
+
+        # Define a non-existing account number
+
+        account_number = 112233
+
+        # Expect a ValueError
+        with self.assertRaises(ValueError) as context:
+            get_balance(account_number)
+
+        # Check that the exception message
+        self.assertEqual(str(context.exception), "Account number does not exist.")
+
+    # Test the make_deposit function with a valid account number and deposit amount
+    def test_make_deposit_correct_balance(self):
+
+        # Define a valid account number
+        account_number = 123456
+
+        # Define a valid deposit amount
+        amount = 1500.01
+        make_deposit(account_number, amount)
+        self.assertEqual(ACCOUNTS[account_number]["balance"], 2500.01)
+
+    # Test the make_deposit function for correct success message
+    def test_make_deposit_correct_output(self):
+
+        # Define a valid account number
+        account_number = 123456
+
+        # Define the deposit amount
+        amount = 1500.01
+        expected_output = "You have made a deposit of $1,500.01 to account 123456."
+        self.assertEqual(make_deposit(account_number, amount), expected_output)
+
+    # Test the make_deposit function when the account does not exist
+    def test_make_deposit_account_does_not_exist(self):
+        
+        # Define a non-existing account number
+        account_number = 112233
+        
+        # Define a deposit amount
+        amount = 1500.01
+        with self.assertRaises(ValueError) as context:
+            make_deposit(account_number, amount)
+        
+        # Check that the exception message
+        self.assertEqual(str(context.exception), "Account number does not exist.")
+
+    # Test the make_deposit function with a negative deposit amount
+    def test_make_deposit_negative(self):
+
+        # Define a non-existing account number
+        account_number = 123456
+
+        # Define a deposit amount
+        amount = -50.01
+        with self.assertRaises(ValueError) as context:
+            make_deposit(account_number, amount)
+        
+        # Check that the exception message
+        self.assertEqual(str(context.exception), "Invalid Amount. Amount must be positive.")
+
+    # Test the user_selection function with a valid task in lowercase
+    @patch('builtins.input')
+    def test_user_selection_valid_lowercase(self, mock_input):
+
+        # Mock user input as "balance"
+        mock_input.side_effect = ["balance"]
+        self.assertEqual(user_selection(), "balance")
+
+     # Test the user_selection function with valid input but in uppercase
+    @patch('builtins.input')
+    def test_user_selection_valid_wrong_case(self, mock_input):
+
+        # Mock user input in uppercase
+        mock_input.side_effect = ["DEPOSIT"]
+        self.assertEqual(user_selection(), "deposit")
+
+    # Test the user_selection function with an invalid task
+    @patch('builtins.input')
+    def test_user_selection_invalid(self, mock_input):
+
+        # Mock an invalid user input
+        mock_input.side_effect = ["invalid_selection"]
+        with self.assertRaises(ValueError) as context:
+            user_selection()
+        
+        # Check that the exception message
+        self.assertEqual(str(context.exception), "Invalid task. Please choose balance, deposit, or exit.")
+
+# Execute all the test cases in the ChatbotTests class.
+if __name__ == '__main__':
+    unittest.main()
+
